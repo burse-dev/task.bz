b45414357d00c47ffc0e43a596f7e4dc7eb4af00
Igor Pershin <igor.pershin.me@gmail.com>
2020-11-29 20:00:38 +0300

    add category types

--- src/constant/category.js
+++ src/constant/category.js
@@ -1,5 +1,7 @@
 export const CLICK_CATEGORY_ID = 1;
 export const COMMENTS_CATEGORY_ID = 2;
+export const REPOST_CATEGORY_ID = 3;
+export const LIKE_CATEGORY_ID = 4;
 
 export default [
   {
@@ -9,5 +11,13 @@ export default [
   {
     id: COMMENTS_CATEGORY_ID,
     name: 'Комментирование',
+  },
+  {
+    id: REPOST_CATEGORY_ID,
+    name: 'Репост записей',
+  },
+  {
+    id: LIKE_CATEGORY_ID,
+    name: 'Лайк',
   },
 ];
